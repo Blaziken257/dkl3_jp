4cdcc8bbbc26dfee399daa397b3c2ece6d573e90
Blaziken257 <2187686+Blaziken257@users.noreply.github.com>
2025-04-24 00:29:29 -0700

    Consistency with constants

--- game/src/bonus.asm
+++ game/src/bonus.asm
@@ -1,29 +1,16 @@
 INCLUDE "registers.inc"
 INCLUDE "constants.asm"
-; RAM addresses
-DEF wStageType EQU $C5ED
-DEF wBonusTimer EQU $C5EE
-DEF wBonusObjCounter EQU $C5EF ; Stars/enemies
-DEF wSoundEffect EQU $DF57
-
-; Level/bonus stage IDS
-DEF M_Bonus_FindToken EQU 0
-DEF M_Bonus_CollectStars EQU 1
-DEF M_Bonus_Bash_Baddies EQU 2
-DEF M_WarpArea EQU 3
-DEF M_LevelAfterBonus EQU 254 ; Normal level after leaving bonus stage
-DEF M_LevelNormal EQU 255
 
 ; TODO: Banana collecting code seems to start at 0:208F
-
+    
 SECTION "Banana/Star Collected", ROM0[$2149]
 BananaCollected:
-    ld   a, [wStageType]
-    cp   M_Bonus_CollectStars ; Check if stage type is Collect the Stars
+    ld   a, [W_Stage_Type]
+    cp   M_Bonus_Collect_Stars ; Check if stage type is Collect the Stars
     ; (Note that this checking is different from DKL2, which checks if not FF. This means that no Collect the Stars stages in that game have enemies
     ; This was addressed in DKL3, and no longer has a bug where collecting bananas after losing a bonus can trigger a Kremkoin chime)
     jr   nz, .NotStarBonus ; Seems to affect when a Bonus Coin can appear (since they're only meant to appear in bonus stages)
-    ld   hl, wBonusObjCounter ; If stage type == Collect the Stars, then check if star/enemy counter is 0 
+    ld   hl, W_Bonus_Obj_Counter ; If stage type == Collect the Stars, then check if star/enemy counter is 0 
     ld   a, [hl]
     and  a ; If not 0, decrement
     jr   z, .NotStarBonus
@@ -33,16 +20,16 @@ BananaCollected:
     and  a
     jr   nz, .NotStarBonus
     ld   a, $0E ; If bonus object counter transitions from 1 to 0, play Bonus Coin chime (same as extra life sound effect)
-    ld   [wSoundEffect], a
+    ld   [W_Sound_Effect], a
 .NotStarBonus:
-    ld   hl, BANANA_COUNTER
+    ld   hl, H_Banana_Counter
     ; TODO: Move the HRAM bytes into a separate file!!
     ld   a, [hl]
     cp   $99 ; 99 bananas; game uses BCDs frequently
     jr   nz, .100bananas
     xor  a
     ld   [hl], a
-    ld   hl, LIFE_COUNTER
+    ld   hl, H_Life_Counter
     ld   a, [hl]
     inc  a
     jr   z, $2103
@@ -66,7 +53,7 @@ EnemyDefeated:
     bit  4, [hl] ; Unsure what this check does; sprite defeated/disappeared?
     ld   l, e
     jr   z, $25C4 ; If check doesn't match, skip rest of code
-    ld   a, [wStageType] ; Check if stage type == Collect the Stars
+    ld   a, [W_Stage_Type] ; Check if stage type == Collect the Stars
 ; BUG: Losing a bonus stage (or soft-resetting during one) doesn't reset the enemy/star counter.
 ; This means that defeating an enemy in either situation will decrement the counter.
 ; Once it reaches zero, the Bonus Coin chime sound effect will play.
@@ -81,18 +68,18 @@ EnemyDefeated:
 ; All versions of DKL2 also has a quirk where this check (stage type != $FF) makes it so that Collect the Stars stages don't have enemies,
 ; and that Destroy them All stages don't have stars.
 ; Since these checks differ in DKL3, this issue is not present in this game.
-    cp   M_Bonus_CollectStars 
+    cp   M_Bonus_Collect_Stars 
     jr   z, $25E9 ; If current stage type != Collect the Stars, skip rest of code
-    ld   a, [wBonusObjCounter] ; Otherwise, check if star (banana) / enemy counter is 0
+    ld   a, [W_Bonus_Obj_Counter] ; Otherwise, check if star (banana) / enemy counter is 0
     and  a 
     jr   z, $25E9 ; If counter is zero, skip rest of code
     sub  1 ; Would be more efficient to use dec a here
     daa  
-    ld   [wBonusObjCounter], a ; If not zero, decrement
+    ld   [W_Bonus_Obj_Counter], a ; If not zero, decrement
     and  a
     jr   nz, $25E9
     ld   a, $0E ; If bonus object counter transitions from 1 to 0, play Bonus Coin chime (same as extra life sound effect)
-    ld   [wSoundEffect], a
+    ld   [W_Sound_Effect], a
     jr   $25E9 
 
 ; 25E9 seems irrelevant to bonus stuff
